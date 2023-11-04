f374f0d6b4c8b9888dc9a0975c2f3aace13fe744
SinWorld7 <105977205+SinWorld7@users.noreply.github.com>
2023-11-04 10:54:54 +0800

    Delete web_auto_test directory

--- web_auto_test/test_data.py
+++
@@ -1,5 +0,0 @@
-url = {"url": "http://erp.lemfix.com/"}
-# login_data = [{"username": "test123"}, {"password": "123456"}]
-login_data = {"username": "test123", "password": "123456"}
-s_key = {"search_key": "9028"}
-
